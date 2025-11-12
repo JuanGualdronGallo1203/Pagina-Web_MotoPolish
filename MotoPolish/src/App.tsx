6327c86fb5a0a7f1baa924509210fdaf11750d7d
JuanGualdron1203 <juangualdron13@gmail.com>
2025-11-12 11:11:03 -0500

    feat: :sparkles: Talwind is installed and configured to work
    Talwind is installed and configured to work

---
+++ MotoPolish/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col gap-5 items-center justify-center bg-blue-500 text-white text-4xl font-bold">
+      Tailwind CSS funcionando ✅
+    </div>
+  )
+}
+

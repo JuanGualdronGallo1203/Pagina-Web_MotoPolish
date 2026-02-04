ad8d12408edf45417d1a835b53cc35a2538666a0
JuanGualdron1203 <juangualdron13@gmail.com>
2026-02-04 17:53:28 -0500

    feat: :bug: fix a bug
    fix a bug

--- MotoPolish/vite.config.ts
+++ MotoPolish/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: 'Pagina-Web_MotoPolish',
+  
   plugins: [
     react(),
     tailwindcss()
